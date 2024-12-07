d6917505fcbb8dde5f5d1a47b21bb1abd29e7ec9
alan-afonso <alanju255@gmail.com>
2024-12-07 01:48:22 -0300

    feat:primeira versao

---
+++ exemplo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+    int a;
+    int b;
+
+
+    cout << "exemplo inicial de apliv=cativo" << endl;
+    cout << "digita um numero: ";
+    cin >> a; 
+    cout << "digite outro numero :";
+    cin >> b;
+    cout <<"a soma dos numero e: " << a + b << endl;
+
+    system("pause");
+    return 0;
+}
