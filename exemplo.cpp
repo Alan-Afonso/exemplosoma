889d7f0cee68bcf4947cbdb0025315e008cae834
alan-afonso <alanju255@gmail.com>
2024-12-07 20:19:10 -0300

    hotfix: adicionando tratamento para espaço em branco

--- exemplo.cpp
+++ exemplo.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
-
+#include <string>
 using namespace std;
 
 
 int main(){
     int a;
     int b;
+    string nome;
 
 
-    cout << "exemplo inicial de apliv=cativo" << endl;
+    cout << "exemplo inicial de aplicativo" << endl;
+    cout << "digite o seu nome: ";
+    //cin >> nome;
+    getline(cin, nome);
     cout << "digita um numero: ";
     cin >> a; 
     cout << "digite outro numero :";
     cin >> b;
-    cout <<"a soma dos numero e: " << a + b << endl;
+    cout << " fale " << nome << ", A soma dos numero e: " << a + b << endl;
 
     system("pause");
     return 0;
